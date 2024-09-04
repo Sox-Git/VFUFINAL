830f4f453916cb9a2f1d4fef610c75a18f318ed9
Sox <113142608+Maz-github@users.noreply.github.com>
2024-09-04 17:27:34 -0400

    Update TitleState.hx

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -379,7 +379,7 @@ class TitleState extends MusicBeatState
 					if (mustUpdate) {
 						MusicBeatState.switchState(new OutdatedState());
 					} else {
-						MusicBeatState.switchState(new MainMenuState());
+						MusicBeatState.switchState(new FreeplayState()); //Send to freeplay for now
 					}
 					closedState = true;
 				});
