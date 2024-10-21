aa2941096037950c548385eecf6727e31ccfdeee
Parker Dunbar <113142608+Sox-Git@users.noreply.github.com>
2024-10-21 09:39:24 -0400

    FPS Counter has Version Number

--- source/debug/FPSCounter.hx
+++ source/debug/FPSCounter.hx
@@ -63,7 +63,7 @@ class FPSCounter extends TextField
 
 	public dynamic function updateText():Void { // so people can override it in hscript
 		text = 'FPS: ${currentFPS}'
-		+ '\nMemory: ${flixel.util.FlxStringUtil.formatBytes(memoryMegas)}';
+		+ '\nMemory: ${flixel.util.FlxStringUtil.formatBytes(memoryMegas)}\nVDEV';
 
 		textColor = 0xFFFFFFFF;
 		if (currentFPS < FlxG.drawFramerate * 0.5)
