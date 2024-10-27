6d4f7454cc0f82aecbf261e1a1fb6c09c0f56f2e
Sox <urmomzam@gmail.com>
2024-10-27 10:51:20 -0400

    Update Character.hx

--- source/objects/Character.hx
+++ source/objects/Character.hx
@@ -103,7 +103,7 @@ class Character extends FlxSprite
 				if (!Assets.exists(path))
 				#end
 				{
-					path = Paths.getSharedPath('images/Game_Assets/Playstate/FNF/CHAR/BF/Default/data/0.json); //If a character couldn't be found, change him to BF just to prevent a crash
+					path = Paths.getSharedPath('images/Game_Assets/Playstate/FNF/CHAR/BF/Default/data/0.json');
 					color = FlxColor.BLACK;
 					alpha = 0.6;
 				}
